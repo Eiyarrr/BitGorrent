f32b8c1afc3841948f8e57b9451b0ea98185c16a
Eiyarrr <eiyar@eiyar.com>
2026-07-27 06:41:37 -0700

    feat: added basic bitfield structure

---
+++ bitfield.go
@@ -0,0 +1,12 @@
+package bittorrent
+
+// bitfields represent what pieces peers have
+type Bitfield []byte
+
+func (bf Bitfield) HasPiece(index int) bool {
+
+}
+
+func (bf Bitfield) SetPiece(index int) {
+
+}
