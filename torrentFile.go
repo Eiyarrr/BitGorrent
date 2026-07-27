17921eec2442f089fdd715efe7288fdcae1e6638
Eiyarrr <eiyar@eiyar.com>
2026-07-27 04:45:02 -0700

    feat: added basic parsing from bencodeTorrent to TorrentFile

--- torrentFile.go
+++ torrentFile.go
@@ -2,19 +2,30 @@ package bittorrent
 
 import (
 	"io"
+	"weak"
+
 	"github.com/Eiyarrr/Bencode-Parser"
 )
 
 type bencodeInfo struct {
-    Pieces      string `bencode:"pieces"`
-    PieceLength int    `bencode:"piece length"`
-    Length      int    `bencode:"length"`
-    Name        string `bencode:"name"`
+	Pieces      string `bencode:"pieces"`
+	PieceLength int    `bencode:"piece length"`
+	Length      int    `bencode:"length"`
+	Name        string `bencode:"name"`
 }
 
 type bencodeTorrent struct {
-    Announce string      `bencode:"announce"`
-    Info     bencodeInfo `bencode:"info"`
+	Announce string      `bencode:"announce"`
+	Info     bencodeInfo `bencode:"info"`
+}
+
+type TorrentFile struct {
+	Announce    string
+	InfoHash    [20]byte
+	PieceHashes [][20]byte
+	PieceLength int
+	Length      int
+	Name        string
 }
 
 func Open(reader io.Reader) (*bencodeTorrent, error) {
@@ -25,4 +36,15 @@ func Open(reader io.Reader) (*bencodeTorrent, error) {
 	}
 
 	return &benTor, nil
+}
+
+func toTorrentFile(benTor bencodeTorrent) (TorrentFile, error) {
+	torrent := TorrentFile{
+		Announce:    benTor.Announce,
+		PieceLength: benTor.Info.PieceLength,
+		Length:      benTor.Info.Length,
+		Name:        benTor.Info.Name,
+	}
+
+	return torrent, nil
 }
