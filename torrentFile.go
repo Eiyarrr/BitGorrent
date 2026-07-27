486972ad9d2643e200d611003187757568925bd5
Eiyarrr <eiyar@eiyar.com>
2026-07-27 04:16:47 -0700

    feat: added structs of bencode fields

---
+++ torrentFile.go
@@ -0,0 +1,13 @@
+package bittorrent
+
+type bencodeInfo struct {
+    Pieces      string `bencode:"pieces"`
+    PieceLength int    `bencode:"piece length"`
+    Length      int    `bencode:"length"`
+    Name        string `bencode:"name"`
+}
+
+type bencodeTorrent struct {
+    Announce string      `bencode:"announce"`
+    Info     bencodeInfo `bencode:"info"`
+}
