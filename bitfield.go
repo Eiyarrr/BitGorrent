b26c820cbf0b2b58f59565009ff8f97ba84af3b7
Eiyarrr <eiyar@eiyar.com>
2026-07-27 06:44:45 -0700

    feat: implemented HasPiece

--- bitfield.go
+++ bitfield.go
@@ -4,7 +4,11 @@ package bittorrent
 type Bitfield []byte
 
 func (bf Bitfield) HasPiece(index int) bool {
+	byteIndex := index/8
+	offset := byteIndex % 8
 
+	// the bitfield in the byte, at the bit, isolate it, return bit != 0
+	return bf[byteIndex]>>(7-offset)&1 != 0
 }
 
 func (bf Bitfield) SetPiece(index int) {
