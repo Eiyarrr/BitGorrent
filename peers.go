98b41ff9e804834b75240f7d1cba41f76dc3463d
Eiyarrr <eiyar@eiyar.com>
2026-07-27 05:24:50 -0700

    feat: buildTrackerURL now generates params

--- peers.go
+++ peers.go
@@ -1,12 +1,25 @@
 package bittorrent
 
-import "net/url"
+import (
+	"net/url"
+	"strconv"
+)
 
 // belongs to torFile (e.g. torFile.buildTrackerURL(...))
 func (torFile *TorrentFile) buildTrackerURL(peerID [20]byte, port uint16) (string, error) {
 	base, err := url.Parse(torFile.Announce)
 	if err != nil {
 		return "", err
+	}
+
+	params := url.Values{
+		"info_hash":  []string{string(torFile.InfoHash[:])},
+		"peer_id":    []string{string(peerID[:])},
+		"port":       []string{strconv.Itoa(int(port))},
+		"uploaded":   []string{"0"},
+		"downloaded": []string{"0"},
+		"compact":    []string{"1"},
+		"left":       []string{strconv.Itoa(torFile.Length)}}
 	}
 
 	return "", nil
