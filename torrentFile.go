3bf07d01eb5cf865a92775a70b0932694fca1908
Eiyarrr <eiyar@eiyar.com>
2026-07-27 04:34:53 -0700

    feat: added function to open and fill bencodeTorrent struct

--- torrentFile.go
+++ torrentFile.go
@@ -1,5 +1,10 @@
 package bittorrent
 
+import (
+	"io"
+	"github.com/Eiyarrr/Bencode-Parser"
+)
+
 type bencodeInfo struct {
     Pieces      string `bencode:"pieces"`
     PieceLength int    `bencode:"piece length"`
@@ -10,4 +15,14 @@ type bencodeInfo struct {
 type bencodeTorrent struct {
     Announce string      `bencode:"announce"`
     Info     bencodeInfo `bencode:"info"`
+}
+
+func Open(reader io.Reader) (*bencodeTorrent, error) {
+	benTor := bencodeTorrent{}
+	err := bencode.Unmarshal(reader, benTor)
+	if err != nil {
+		return nil, err
+	}
+
+	return &benTor, nil
 }
