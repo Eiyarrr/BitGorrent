454f6677b4c6f4a783020f6a33f11d6ebc0d7de1
Eiyarrr <eiyar@eiyar.com>
2026-07-28 02:08:13 -0700

    feat: added reading from file at path and added parse helper function for turning []byte into *Torrent

--- torrent/load.go
+++ torrent/load.go
@@ -1,6 +1,9 @@
 package torrent
 
-import "net/url"
+import (
+	"net/url"
+	"os"
+)
 
 func Load(pathOrURL string) (*Torrent, error) {
 	// crazy permissive, local files are also *url.URL
@@ -22,5 +25,14 @@ func loadURL(u *url.URL) (*Torrent, error) {
 }
 
 func loadPath(path string) (*Torrent, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, nil
+	}
+
+	return parse(data)
+}
+
+func parse(data []byte) (*Torrent, error) {
 	return nil, nil
 }
