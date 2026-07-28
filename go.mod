5086adaee39f28c1e5cbddb86993d461640d8f30
Eiyarrr <eiyar@eiyar.com>
2026-07-28 03:12:35 -0700

    feat: added test dir (and ignored) for testing and updated require to use most recent bencode parser on my github

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/Eiyarrr/BitGorrent
 
 go 1.26.5
 
-require github.com/Eiyarrr/Bencode-Parser v0.0.0-20260727120754-bf62452b7b2c // indirect
+require github.com/Eiyarrr/Bencode-Parser v0.0.0-20260728091457-687beaafb161 // indirect
--- main.go
+++ main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/Eiyarrr/BitGorrent/client"
+	"github.com/Eiyarrr/BitGorrent/torrent"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
 	c := client.New()
 	c.Run()
+
+	tor, err := torrent.Load("testTorrents/debian-edu-13.6.0-amd64-netinst.iso.torrent")
+	if err != nil {
+		fmt.Println("Error loading torrent")
+	}
+
+	fmt.Println("heres the torrent")
+	fmt.Println(tor)
 }
